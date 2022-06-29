718a3b1e14572b2b0f903d23dfe8732461c1cfbf
xxseva44 <dev.xxseva44@gmail.com>
2022-06-28 22:21:37 -0400

    Update sound trigger to V2.1

--- msm8996.mk
+++ msm8996.mk
@@ -83,6 +83,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio.service \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
@@ -92,6 +93,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.common@6.0-util \
     android.hardware.audio.effect@6.0 \
     android.hardware.audio.effect@6.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
