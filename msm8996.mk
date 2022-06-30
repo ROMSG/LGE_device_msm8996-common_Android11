6856c2d85950966495cc8e5c88494b910afe5170
ROMSG <48342780+ROMSG@users.noreply.github.com>
2022-06-29 23:37:30 -0700

    Nuke LGE_Settings for now
    Not currently working

--- msm8996.mk
+++ msm8996.mk
@@ -154,10 +154,6 @@ PRODUCT_PACKAGES += \
     libdisplayconfig \
     libqdMetaData.system
 
-# LGE settings
-PRODUCT_PACKAGES += \
-    LGESettings
-
 # Doze mode
 PRODUCT_PACKAGES += \
     LGDoze
